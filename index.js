813cfe0c2747e884add4e1607a5548a1a4cc6672
joenovotny <161538874+joenovotny@users.noreply.github.com>
2025-04-16 08:42:19 -0400

    updated

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const options = {
     keyId: process.env.APNS_KEY_ID,
     teamId: process.env.APNS_TEAM_ID,
   },
-  production: true,
+  production: false,
 };
 
 const apnProvider = new apn.Provider(options);
